4536e9ed2be3ddf74ba9d070e463823521a736ad
Gaurav Wagh <Gauravwa9806@gmail.com>
2025-06-09 01:22:25 +0530

    2D Array Assignment

--- 2dArray.c
+++ 2dArray.c
@@ -87,7 +87,70 @@ void main()
             }
         }
     }
-    printf("Minimum and Maximum: -\nMin: - %d \nMax: - %d", min, max);
+    printf("Minimum and Maximum: -\nMin: - %d \nMax: - %d\n", min, max);
+
+    printf("\n-------------------------\n");
+
+    // LEFT Diagonal elements
+    int arr1[3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            // if ((i == 0 && j == 0) | (i == 1 && j == 1) | (i == 2 && j == 2))
+            if ((i - j) == 0)
+            {
+                arr1[i] = arr[i][j];
+            }
+        }
+    }
+    printf("\nLeft Diagonal Elements: -\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int z = 0; z < (0 + i) * 2; z++)
+        {
+            printf(" ");
+        }
+        printf("%d\n", arr1[i]);
+    }
+
+    printf("\n-------------------------\n");
+
+    // Right Diagonal elements
+    int arr2[3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            // if ((i == 0 && j == 2) | (i == 1 && j == 1) | (i == 2 && j == 0))
+            if ((i + j) == 2)
+            {
+                arr2[i] = arr[i][j];
+            }
+        }
+    }
+
+    printf("\nRight Diagonal Elements: -\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int z = 0; z < (2 - i) * 2; z++)
+        {
+            printf(" ");
+        }
+        printf("%d\n", arr2[i]);
+    }
+
+    printf("\n-------------------------\n");
+
+    // L Elements in 2D Array
+    printf("L Elements: -\n\n");
+    printf("%d\n%d\n%d %d %d\n", arr[0][0], arr[1][0], arr[2][0], arr[2][1], arr[2][2]);
+
+    printf("\n-------------------------\n");
+
+    // N Elements 2D Array
+    printf("N Elements: -\n\n");
+    printf("%d   %d\n%d %d %d\n%d   %d\n", arr[0][0], arr[0][2], arr[1][0], arr[1][1], arr[1][2], arr[2][0], arr[2][2]);
 
     printf("\n-------------------------\n");
 }
